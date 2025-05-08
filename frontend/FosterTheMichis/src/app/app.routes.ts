2d3fb14ea9e67ef28605093eb49ad488c31003f4
avargss <alevargasgarran@gmail.com>
2025-05-08 17:11:08 +0200

    Home terminado

--- frontend/FosterTheMichis/src/app/app.routes.ts
+++ frontend/FosterTheMichis/src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { AdopcionComponent } from './components/adopcion/adopcion.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, title: 'Home' },
     { path: 'login', component: LoginComponent, title: 'Login' },
+    { path: 'menu', component: MenuComponent, title: 'Menú' },
+    { path: 'galeria', component: GaleriaComponent, title: 'Galería' },
+    { path: 'adopcion', component: AdopcionComponent, title: 'Adopción' },
+    { path: 'contacto', component: ContactoComponent, title: 'Contacto' },
+    { path: 'reservas', component: ReservasComponent, title: 'Reservas' },
+    
 ];
