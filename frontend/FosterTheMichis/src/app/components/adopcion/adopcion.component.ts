17c320d8df2626ab07cc9d3f388801f2bb353d4d
avargss <alevargasgarran@gmail.com>
2025-05-19 23:30:29 +0200

    Galeria terminada. Adopción en proceso

--- frontend/FosterTheMichis/src/app/components/adopcion/adopcion.component.ts
+++ frontend/FosterTheMichis/src/app/components/adopcion/adopcion.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Michi } from '../../model/michis';
+import { MichisService } from '../../services/michis.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-adopcion',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './adopcion.component.html',
   styleUrl: './adopcion.component.css'
 })
 export class AdopcionComponent {
 
+  nonAdoptableMichis: Michi[] = [];
+  adoptableMichis: Michi[] = [];
+
+  constructor(private michisService: MichisService) { }
+
+  ngOnInit() {
+    this.loadNonAdoptableMichis();
+    this.loadAdoptableMichis();
+  }
+
+  // Método para cargar los gatos no adoptables desde la base de datos
+  loadNonAdoptableMichis(): void {
+    this.michisService.getNonAdoptableMichis().subscribe((michis) => {
+      this.nonAdoptableMichis = michis;
+    });
+  }
+
+  // Método para cargar los gatos adoptables desde la base de datos
+  loadAdoptableMichis(): void {
+    this.michisService.getAdoptableMichis().subscribe((michis) => {
+      this.adoptableMichis = michis;
+    });
+  }
 }
--- frontend/FosterTheMichis/src/app/components/galeria/galeria.component.ts
+++ frontend/FosterTheMichis/src/app/components/galeria/galeria.component.ts
@@ -1,29 +1,21 @@
 import { Component } from '@angular/core';
 import { Michi } from '../../model/michis';
 import { MichisService } from '../../services/michis.service';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-galeria',
-  imports: [NgFor, NgIf],
+  imports: [NgFor],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.css'
 })
 export class GaleriaComponent {
-  nonAdoptableMichis: Michi[] = [];
   adoptableMichis: Michi[] = [];
 
   constructor(private michisService: MichisService) {}
 
   ngOnInit() {
-    this.loadNonAdoptableMichis();
-  }
-
-  // Método para cargar los gatos no adoptables desde la base de datos
-  loadNonAdoptableMichis(): void {
-    this.michisService.getNonAdoptableMichis().subscribe((michis) => {
-      this.nonAdoptableMichis = michis;
-    });
+    this.loadAdoptableMichis();
   }
 
   // Método para cargar los gatos adoptables desde la base de datos
